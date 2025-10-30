b8543708ce9840d31ff3750e4376f806a8e333cd
cc3265a <cc3265a@american.edu>
2025-10-30 18:35:43 -0400

    push

--- 08-server-world/api-test.js
+++ 08-server-world/api-test.js
@@ -1,3 +1,4 @@
+import { parse } from "csv-parse/sync";
 
 const SHEET_URL = `https://docs.google.com/spreadsheets/d/1Agr5DA3TzcbqHK5W8HCWzdwRkjZbT9KxzFgbqE_ZoPQ/export?format=csv`;
 
@@ -6,25 +7,37 @@ async function fetchAndParseSheet(){
         const res = await fetch(SHEET_URL);
         const text = await res.text();
 
-        let lines = text.split("\n");
-        let headers = lines[0].split(",");
-        let studentInfo[];
+        // let lines = text.split("\n");
+        // let headers = lines[0].split(",");
+        // let studentInfo = [];
 
-        for (let i = 0; i < lines.length; i++){
-            let line = lines[i].trim();
-            let entries = line.split(",");
-            let student = {};
-            for (let j = 0; j < entries.length; j++){
-                student[headers[j]] = entries[j];
-            }
-            studentInfo.push(student);
-        }
+        // for (let i = 0; i < lines.length; i++){
+        //     let line = lines[i].trim();
+        //     let entries = line.split(",");
+        //     let student = {};
+        //     for (let j = 0; j < entries.length; j++){
+        //         student[headers[j]] = entries[j];
+        //     }
+        //     studentInfo.push(student);
+        // }
 
 
-        console.log(studentInfo);
+        // console.log(studentInfo);
+
+        const lines = text.trim().split("\n");
+        const headers = lines[0].split(",");
+
+        const data = lines.slice(1).map(line => {
+        const values = line.split(",");
+        return Object.fromEntries(headers.map((h, i) => [h, values[i]]));
+        });
+
+
     } catch {
 
     }
 }
+
+
 
 fetchAndParseSheet();
--- 08-server-world/server.js
+++ 08-server-world/server.js
@@ -1,20 +1,14 @@
 import express from "express";
 import fs from "fs";
 import cors from "cors";
+
 const app = express();
 
-// This line will make it so the node server will make the "front end" (html, css, js)
-// is served by the server. No more Live-server!
 app.use(express.static("./public"));
-
-// This line helps us process requests that contain json (e.g. in the '/excite' 
-// route req.data is automatically json because of this)
 app.use(express.json());
-
 app.use(cors());
 
 app.get("/world", async (req, res) => {
-    // Read in the data, parse it into an object and send it over as json to the client
     const dataString = await fs.readFileSync("world.json", "utf-8");
     const dataObject = JSON.parse(dataString);
     res.json(dataObject);
