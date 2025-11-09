c17255f03e84ff5118d407f0e2ad4217ccfade16
cc3265a <cc3265a@american.edu>
2025-11-09 16:22:52 -0500

    hhhh

--- 09-react-basics/src/App.jsx
+++ 09-react-basics/src/App.jsx
@@ -2,57 +2,92 @@ import { useState } from 'react'
 import TodoItem from "./TodoItem"
 
 function App() {
-  // let x = 10;
-  // useState allows it to be updated in render whenever it changes
-  const [x, setX] = useState(10);
-  const [name, setName] = useState("Mike");
-  
+  // const [x, setX] = useState(10);
+
+  // ARRAY DESTRUCTURING with useState:
+  // useState("") returns an array: ["", function]
+  // The square brackets [task, setTask] use "array destructuring" to assign:
+  //   - task = the first element (the current state value)
+  //   - setTask = the second element (a function to update the state)
+  // Instead of writing: const stateArray = useState(""); const task = stateArray[0]; const setTask = stateArray[1];
+  // We use destructuring to get both values at once: const [task, setTask] = useState("");
   const [task, setTask] = useState("");
+  const [type, setType] = useState("");
+  
+  // Another useState hook - this time for an array of todo objects
   const [todos, setTodos] = useState([
     {
-      task: "eat dinner",
-      complete: false,
+      task: "Encyclopedia of Fairies",
+      type: "single",
       id: 1
     },
     {
-      task: "pet cat",
-      complete: true,
+      task: "The Martian",
+      type: "single",
       id: 2
     },
     {
-      task: "play game",
-      complete: false,
+      task: "The Lost Tomb",
+      type: "series",
       id: 3
     }
   ]);
 
 
-  function addTodo(){
-    console.log(task);
-    let newTodo = {
+  function addTodo() {
+    // Create a new todo object with the current task value
+    let newBook = {
       task: task,
-      completed: false,
-      id: Date.now()
+      type: type,
+      id: Date.now()    // Generate a unique ID using current timestamp
     }
-    setTodos(...todos, newTodo)
+    // SPREAD OPERATOR (...todos):
+    // Creates a new array containing all existing todos, then adds newTodo
+    // We must create a new array (not modify the old one) because React needs to detect changes
+    // [1, 2, 3] becomes [1, 2, 3, newTodo] - keeps all old items, adds the new one
+    setTodos([...todos, newBook]);
+    // Clear the input field by resetting task to empty string
     setTask("");
+    setType("");
+    // console.log(todos);
   }
 
+  // JSX (JavaScript XML) - looks like HTML but it's actually JavaScript
+  // The return statement returns JSX that React will render to the page
   return (
     <div>
+      <label for="bookNameInp">Book Name </label>
       <input 
         type="text"
-        onChange={(e) => setTask(e.target.value)}
+        value={task}
+        id="bookNameInp"
+        onChange={(e) => setTask(e.target.value)} 
       />
-      <button onClick={addTodo}>Add Todo</button>
-
-      {task}
+      <br></br>
+      <label for="bookTypeImp">Book Type (single or series) </label>
+      <input 
+        type="text"
+        value={type}
+        id="bookTypeImp"
+        onChange={(e) => setType(e.target.value)} 
+      />
+      <br></br>
+      <button onClick={addTodo}>Add Book to the To-Read List</button>
+      
 
       <ul>
+        {/* .map() loops through the todos array and creates a component for each one */}
+        {/* { } curly braces mean "this is JavaScript code" inside JSX */}
+        {/* todos.map((todo) => ...) means: for each todo in todos, do something */}
+        {/* The arrow function receives each todo object and returns a TodoItem component */}
+        {/* key={todo.id} is required by React - helps React track which items changed */}
         {todos.map((todo) => (
-          <TodoItem todo={todo} key={todo.id}/>
+          <TodoItem 
+          todo={todo}
+          key={todo.id}/>
         ))}
       </ul>
+
     </div>
   )
 }
--- 09-react-basics/src/TodoItem.jsx
+++ 09-react-basics/src/TodoItem.jsx
@@ -1,13 +1,32 @@
-import { useState } from 'react'
+// Import CSS file for styling this component
 import "./TodoItem.css"
 
-function TodoItem(todo) {
-  
+// OBJECT DESTRUCTURING in function parameters:
+// Instead of: function TodoItem(props) { const todo = props.todo; ... }
+// We use destructuring to extract 'todo' directly from the props object: {todo}
+// This is similar to array destructuring, but uses curly braces {} for objects
+// This means the component expects to receive a prop called "todo"
+// Props are data passed from parent component (App) to child component (TodoItem)
+function TodoItem({todo}) {
+  // JSX return - renders a single list item
   return (
-    <li style={{textDecoration: todo.complete ? "line-through" : "none"}}>
+    <li 
+        // Inline styles use double curly braces {{ }}:
+        // Outer {} = "this is JavaScript in JSX"
+        // Inner {} = "this is a JavaScript object"
+        style={
+            {
+                // Ternary operator: condition ? valueIfTrue : valueIfFalse
+                // If todo.complete is true, show line-through, otherwise show none
+                // textDecoration: todo.complete ? "line-through" : "none",
+                color: (todo.type === "series") ? "blue" : "purple"
+            }
+        }>
+        {/* {todo.task} means "display the value of todo.task here" */}
         {todo.task}
     </li>
   )
 }
 
+// Export makes this component available for other files to import
 export default TodoItem
