3a465a5d6aecf2a68da94a86eadb032b401afb3d
cc3265a <cc3265a@american.edu>
2025-12-02 19:08:47 -0500

    cors

--- final/README.md
+++ final/README.md
@@ -1,3 +1,3 @@
 It's Rin's Final
 
-Here is a [link]("put link here .com")
+Here is a [link](https://csc336-fall2025-1.onrender.com)
--- final/server/index.js
+++ final/server/index.js
@@ -1,7 +1,9 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/api/data", (req, res) => {
     console.log("hello from api endpoint /api/data");
